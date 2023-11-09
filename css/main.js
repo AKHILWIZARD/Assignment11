c0e1b43e4fe0b7cec95d3b25719dc5149f242ce5
AKHILWIZARD <arunakhil978@gmail.com>
2023-11-09 21:29:12 +0530

    first

---
+++ css/main.js
@@ -0,0 +1,14 @@
+
+
+
+function dropdownbtn(){
+    
+    var list = document.getElementById('list');
+    if (list.style.display === "none") {
+        list.style.display = "block";
+    } 
+    else {
+        list.style.display = "none";
+    }
+
+}
